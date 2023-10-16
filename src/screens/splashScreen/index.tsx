77b89fb49d35012d42e19d03af535bc836830e0f
dean blinder <deanb@wix.com>
2023-10-17 00:04:30 +0300

    added spinet in splash screen

--- src/screens/splashScreen/index.tsx
+++ src/screens/splashScreen/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import {View, Text} from "native-base";
-import LottieView from 'lottie-react-native';
 
-const lottie = require('../../../assets/lottie/animation_lnt89a60.json')
+import { Spinner, View} from "native-base";
+// import LottieView from 'lottie-react-native';
+
+// const lottie = require('../../../assets/lottie/animation_lnt89a60.json')
 
 const SplashScreen = () => {
     return (
-        <View marginTop={'20%'} justifyContent={'center'} alignItems={'center'}>
-            <LottieView
-                style={{width:'100%',height:'100%'}}
-                source={lottie}
-                autoPlay
-                loop
-            ></LottieView>
-            <Text></Text>
+        <View flex justifyContent={'center'} alignItems={'center'}>
+            <Spinner size={'lg'}/>
+                {/*<LottieView*/}
+                {/*    style={{width:'100%',height:'100%'}}*/}
+                {/*    source={lottie}*/}
+                {/*    autoPlay*/}
+                {/*    loop*/}
+                {/*/>*/}
         </View>
     )
 }
